c7397fa219ad1152496b99646be85bb033d75d2a
sb <santosh.bhushan@mckay-brothers.com>
2018-04-25 17:15:29 -0700

    Exceptions examples

---
+++ hackerR/src/thirty_days_code/day16/Solution.java
@@ -0,0 +1,21 @@
+package thirty_days_code.day16;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try {
+        	int value = Integer.parseInt(S);
+        	System.out.println(value);
+        } catch (Exception ex) {
+        	System.out.println("Bad String");
+        }
+    }
+}
